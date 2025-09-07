626312f928449131ba8b1e8a7dc26deef0806fa6
Miroslav Zeleny <m.zeleny83@gmail.com>
2025-09-07 12:07:40 +0200

    Move found rides box above the map and include other changes

--- app/models.py
+++ app/models.py
@@ -1,9 +1,31 @@
 from . import db
+import datetime
 
-class Book(db.Model):
+class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), unique=True, nullable=False)
-    author = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=True)
+    password_hash = db.Column(db.String(120), nullable=False)
+    rating = db.Column(db.Float, default=5.0)
+    home_city = db.Column(db.String(120), nullable=True)
+    paypal_email = db.Column(db.String(120), nullable=True)
+    created_at = db.Column(db.DateTime, default=db.func.now())
 
     def __repr__(self):
-        return f'<Book {self.title}>'
+        return f'<User {self.name}>'
+
+class DirectMessage(db.Model):
+    __tablename__ = 'direct_messages'
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    message_content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    sender = db.relationship('User', foreign_keys=[sender_id], backref='sent_messages')
+    receiver = db.relationship('User', foreign_keys=[receiver_id], backref='received_messages')
+
+    def __repr__(self):
+        return f'<DirectMessage {self.id} from {self.sender_id} to {self.receiver_id}>'
